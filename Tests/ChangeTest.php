a4e89eb139d2d7c8f3a28320856feae89f879f98
Brendan Grubb and Philip Putnam <philip@>
2017-02-14 16:43:01 -0800

    add calculateChange method

--- Tests/ChangeTest.php
+++ Tests/ChangeTest.php
@@ -3,17 +3,59 @@
 
     class ChangeTest extends PHPUnit_Framework_TestCase
     {
-        function test_quarterChange()
+        function test_calculateChange_quartersChange()
         {
 
           //arrange
-          $change = new Change(0.25);
+          $change = new Change(0.50);
 
           //act
           $result = $change->calculateChange();
 
           //assert
-          $this->assertEquals("Quarter", $result);
+          $this->assertEquals("Quarters: " . 2 . " Dimes: " . 0 . " Nickels: " . 0 . " Pennies: " . 0, $result);
+
+        }
+
+        function test_calculateChange_dimesQuartersChange()
+        {
+
+          //arrange
+          $change = new Change(0.70);
+
+          //act
+          $result = $change->calculateChange();
+
+          //assert
+          $this->assertEquals("Quarters: " . 2 . " Dimes: " . 2 . " Nickels: " . 0 . " Pennies: " . 0, $result);
+
+        }
+
+        function test_calculateChange_nickelsDimesQuartersChange()
+        {
+
+          //arrange
+          $change = new Change(0.65);
+
+          //act
+          $result = $change->calculateChange();
+
+          //assert
+          $this->assertEquals("Quarters: " . 2 . " Dimes: " . 1 . " Nickels: " . 1 . " Pennies: " . 0, $result);
+
+        }
+
+        function test_calculateChange_penniesNickelsDimesQuartersChange()
+        {
+
+          //arrange
+          $change = new Change(0.69);
+
+          //act
+          $result = $change->calculateChange();
+
+          //assert
+          $this->assertEquals("Quarters: " . 2 . " Dimes: " . 1 . " Nickels: " . 1 . " Pennies: " . 4, $result);
 
         }
 
--- src/Change.php
+++ src/Change.php
@@ -2,24 +2,40 @@
     class Change
     {
         private $amount;
+        private $quarters;
+        private $dimes;
+        private $nickels;
+        private $pennies;
         private $correct_change;
 
         function __construct($amount) {
             $this->amount = $amount;
-            $this->correct_change = "";
+            $this->quarters = 0;
+            $this->dimes = 0;
+            $this->nickels = 0;
+            $this->pennies = 0;
         }
 
         function calculateChange()
         {
           $user_change_due = $this->amount;
-          if ($user_change_due >= 0.25) {
-              $user_change_due -= 0.25;
-              if (!$this->correct_change) {
-                $this->correct_change .= "Quarter";
+          while ($user_change_due >= 0.009) {
+              if ($user_change_due > 0.24) {
+                  $user_change_due -= 0.25;
+                  $this->quarters += 1;
+              } elseif ($user_change_due > 0.09) {
+                  $user_change_due -= 0.10;
+                  $this->dimes += 1;
+              } elseif ($user_change_due > 0.04) {
+                  $user_change_due -= 0.05;
+                  $this->nickels += 1;
+              } elseif ($user_change_due > 0) {
+                  $user_change_due -= 0.010;
+                  $this->pennies += 1;
               }
           }
 
-          return $this->correct_change;
+          return "Quarters: " . $this->quarters . " Dimes: " . $this->dimes . " Nickels: " . $this->nickels . " Pennies: " . $this->pennies;
         }
     }
 
